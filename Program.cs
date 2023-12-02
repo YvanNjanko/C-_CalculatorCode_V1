5ae88233a67892098d21229c36b244f830efa1c3
Yvan Njanko <yvannjanko04@gmail.com>
2023-12-02 16:47:00 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Calculatrice
+{
+    static void Main(string[] args)
+    {
+        while (true) {
+            Console.WriteLine("Entrez une opération :");
+            string operation = Console.ReadLine();
+
+            double resultat = EvaluerOperation(operation);
+
+            Console.WriteLine("Résultat : " + resultat);
+        }
+        
+
+    }
+
+    static double EvaluerOperation(string operation)
+    {
+     
+            // Supprimer les espaces de l'opération
+            operation = operation.Replace(" ", "");
+
+            var dataTable = new System.Data.DataTable();
+            var result = dataTable.Compute(operation, "");
+
+            return Convert.ToDouble(result);
+  
+       
+    }
+}
